61b69b5f9ef26cecb881844c504256a73cd8b8f4
Janan Patel <jananpatel2002@gmail.com>
2022-12-28 00:05:42 -0500

    added try catch

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,34 +1,22 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
 import pandas
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 # TODO 1. Create a dictionary in this format:
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
+data.iterrows()
 
 alphabet_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def start_loop():
+    name = input("Type in a name to get NATO alphabet for: ").upper()
+    try:
+        nato_list = [alphabet_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters are acceptable")
+        start_loop()
+    else:
+        print(nato_list)
 
-name = input("Type in a name to get NATO alphabet for: ").upper()
 
-nato_list = [alphabet_dict[letter] for letter in name]
-print(nato_list)
+start_loop()
